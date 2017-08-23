3aab1d73c4df6277281f3edf0a85b6ba03bc6749
Hans Yadav <hansy@users.noreply.github.com>
2017-08-23 06:33:14 +0200

    remove _site

--- _site/it/publi.sh
+++
@@ -1,7 +0,0 @@
-# sh publi.sh
-rm -rf _site/
-bundle exec jekyll build
-git add --all
-git commit -m "`date`"
-git push origin master
-git push origin `git subtree split --prefix _site/ master`:gh-pages --force
