2afbc1a0acf61b81b169ce7b04ba72a2b6a8cc54
Hans Yadav <hansy@users.noreply.github.com>
2017-08-25 03:27:42 +0200

    add site

---
+++ _site/de/publi.sh
@@ -0,0 +1,8 @@
+# sh publi.sh
+git checkout site
+rm -rf _site/
+bundle exec jekyll build
+git add --all
+git commit -m "`date`"
+git push origin site
+git push origin `git subtree split --prefix _site/ site`:gh-pages --force
