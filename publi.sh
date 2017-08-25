a973972da763ca95008145b714fe187c2438c52e
Hans Yadav <hansy@users.noreply.github.com>
2017-08-25 03:20:29 +0200

    Fri Aug 25 03:20:29 CEST 2017

--- publi.sh
+++ publi.sh
@@ -1,8 +1,8 @@
 # sh publi.sh
-git checkout master
+git checkout site
 rm -rf _site/
 bundle exec jekyll build
 git add --all
 git commit -m "`date`"
-git push origin master
-git push origin `git subtree split --prefix _site/ master`:gh-pages --force
+git push origin site
+git push origin `git subtree split --prefix _site/ site`:gh-pages --force
